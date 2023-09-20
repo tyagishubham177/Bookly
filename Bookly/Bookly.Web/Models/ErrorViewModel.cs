eea0a7f372e7a9fc8cf7e1ad6e73ebbb46f0eaa4
shubham tyagi <shubham.tyagi@soprasteria.com>
2023-09-20 12:21:18 +0530

    Web Proj Skeleton added

---
+++ Bookly/Bookly.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bookly.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
